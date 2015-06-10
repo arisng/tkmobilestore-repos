92117f28bed1173db898a490c73d128af403ce88
nda1291 <nda1291@outlook.com>
2015-06-10 23:47:52 +0700

    added EntityFramework  in TKMobileStore.Data project

---
+++ TKMobileStore/TKMobileStore.Data/Mapping/Catalog/ProductMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKMobileStore.Entities.Catalog;
+
+namespace TKMobileStore.Data.Mapping.Catalog
+{
+    public partial class ProductMap : EntityTypeConfiguration<Product>
+    {
+
+    }
+}
